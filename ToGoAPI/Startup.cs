a84246c15bd726a4ac8dc0d060f33abbfb8f1dd4
hodachalliv <hodachalliv@upmc.edu>
2019-05-01 13:39:35 -0400

    cors with certain website restrictions

--- ToGoAPI/Startup.cs
+++ ToGoAPI/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Web.Cors;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Owin;
 
 [assembly: OwinStartup(typeof(ToGoAPI.Startup))]
@@ -13,7 +15,29 @@ namespace ToGoAPI
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             ConfigureAuth(app);
-            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            // app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            var policy = new CorsPolicy()
+            {
+                AllowAnyHeader = true,
+                AllowAnyMethod = true,
+                SupportsCredentials = true
+            };
+
+            // list of domains that are allowed can be added here
+            policy.Origins.Add("https://localhost:44326/");
+            policy.Origins.Add("https://localhost:44348");
+
+            //be sure to include the port:
+            //example: "http://localhost:8081"
+
+            app.UseCors(new CorsOptions
+            {
+                PolicyProvider = new CorsPolicyProvider
+                {
+                    PolicyResolver = context => Task.FromResult(policy)
+                }
+            });
+
         }
     }
 }
